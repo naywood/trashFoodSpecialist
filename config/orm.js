a96d3d227833e2622111227ba257f60156d3da53
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-06-10 17:57:24 -0700

    fixing orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,10 +1,42 @@
 var connection =  require('./connnections.js');
  
-var orm = {
-    all: function (tableInput, cb){
-        connection.query('SELECT * FROM' +tableInput+ ';', function(err, result){
-            if(err) throw err;
-            cb(result)
-        })
+const orm = {
+    selectAll: function (tableName) {
+        const query = "SELECT * FROM ??";
+        return new Promise((resolve, reject) => {
+            connection.query(query, [tableName], (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    },
+    insertOne: function (tableName, obj) {
+        const query = `INSERT INTO BURGERS SET ?`;
+        return new Promise((resolve, reject) => {
+            connection.query(query, obj, (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
+    },
+    updateOne: function (tableName, updCol, updVal, idCol, objId) {
+        const query = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+        return new Promise((resolve, reject) => {
+            connection.query(query, [tableName, updCol, updVal, idCol, objId], (err, results) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(results);
+                }
+            });
+        });
     }
-} 
+};
+
+module.exports = orm;
