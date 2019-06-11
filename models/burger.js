bceca1eccb41682b57c4ae791e370b738aac3208
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-06-10 18:00:05 -0700

    burger.js

--- models/burger.js
+++ models/burger.js
@@ -1,11 +1,44 @@
 var orm =  require ('../config/orm.js');
 
-var burger = {
-    all: function(cb){
-        orm.all('burgers', function(res){
-            cb(res);
-        })
-    }
+function Burger(name) {
+    this.name = name;
+    this.devoured = false;
 }
 
-module.exports = burger;
+Burger.selectBurgers = function () {
+    return new Promise((resolve, reject) => {
+        orm.selectAll("BURGERS").then(results => {
+            resolve(results);
+        }).catch(() => {
+            reject("Could not retrieve burgers");
+        });
+    });
+};
+
+Burger.create = function (burger) {
+    return new Promise((resolve, reject) => {
+        orm.insertOne("BURGERS", {
+            burger_name: burger.name,
+            devoured: burger.devoured
+        }).then(results => {
+            // Get db generated ID
+            burger.id = results.insertId;
+            resolve(burger.id);
+        }).catch(() => {
+            reject("Could not add burger");
+        });
+    });
+};
+
+Burger.updateDevoured = function (burgerId) {
+    return new Promise((resolve, reject) => {
+        orm.updateOne("BURGERS", "DEVOURED", true, "ID", burgerId).then(results => {
+            resolve(results);
+        }).catch(() => {
+            reject("Could not update burger");
+        });
+    })
+};
+
+
+module.exports = Burger;
