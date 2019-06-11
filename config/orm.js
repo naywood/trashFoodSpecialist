2a8c6cf039ce32c4b5b9fc03a1d2b0aa91727de6
NAYWOOD <naywood@Naomis-MacBook-Pro.local>
2019-06-10 17:29:46 -0700

    base content added to orm.js

---
+++ config/orm.js
@@ -0,0 +1,10 @@
+var connection =  require('./connnections.js');
+ 
+var orm = {
+    all: function (tableInput, cb){
+        connection.query('SELECT * FROM' +tableInput+ ';', function(err, result){
+            if(err) throw err;
+            cb(result)
+        })
+    }
+}
